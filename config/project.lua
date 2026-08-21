4d89ecda2fc5eb9a9c3bc50fde11b47b335df3d7
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-21 13:24:30 +0300

    convert config to lua

---
+++ config/project.lua
@@ -0,0 +1,10 @@
+-- project definitions
+NAME = "demos-python-bioinformatics"
+DESCRIPTION_SHORT = "Demos bioinformatics in the python language"
+KEYWORDS = {
+    "bioinformatics",
+    "genomics",
+    "python",
+    "biopython",
+    "sequence-analysis",
+}
